5c91d0af14eadc8a404772e3609679c183efdcc5
jonhjio <50450285+jonhjio@users.noreply.github.com>
2020-07-29 14:28:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,9 +19,9 @@ bot.on("guildCreate", function(guild) {
     console.log(`[guildCreate, info] I was just added to ${guild.name}`)
     const embed = new Discord.RichEmbed()
         .setAuthor(bot.user.username, bot.user.avatarURL)
-        .addField(`:wave: Welcome!`, `Thanks for adding TTGProtect to your server! You might be wondering, how do I setup TTGProtect? Well, I'll show you.`)
+        .addField(`:wave: Welcome!`, `Thanks for adding DDXProtect to your server! You might be wondering, how do I setup DDXProtect? Well, I'll show you.`)
         .addField(`Getting Started`, `To get started, make sure you create a role called "Staff". This role will enable you moderation features such as kick/ban etc. Next, you might want to give this role to other staff members who will be able to take moderation features!`)
-        .addField(`Anything Else?`, `If there is, you can always join the support server or DM a Developer. Developers you can message include ` + "`Ken#4234, haydenbrown39#3169`" + `. The support server should be in the ` + "`about`" + ` command`)
+        .addField(`Anything Else?`, `If there is, you can always join the support server or DM a Developer. Developers you can message include ` + "`💻 𝕊𝕡𝕣𝕒𝕪 💻#0666`" + `. The support server should be in the ` + "`about`" + ` command`)
         .setColor(0x00ff00)
         .setFooter(`Thanks for adding ${bot.user.username}, I wish you luck!`)
     
